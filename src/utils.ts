1e86de98af369eb2c02ae3454bfbc18613362c35
xvvhang <xvvhang@gmail.com>
2024-03-14 12:14:38 +0800

    chore: update assets and lints

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,6 @@
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 export function getTimeStamp(value: string | Date, format: string): string {
-  value = value || new Date()
-  return dayjs(value).format(format)
+  value = value || new Date();
+  return dayjs(value).format(format);
 }
-
